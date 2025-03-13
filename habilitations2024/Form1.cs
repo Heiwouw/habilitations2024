a665989d7f79d2178125d53090289a0c76a1b2ae
Maud <maudanimaux@gmail.com>
2025-03-13 17:37:39 +0100

    Liaison BDD

--- habilitations2024/Form1.cs
+++ habilitations2024/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace habilitations2024
 {
@@ -15,6 +17,35 @@ namespace habilitations2024
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Chaîne de connexion à la base de données
+            string connectionString = "Server=localhost;Database=habilitations;User Id=habilitations;Password=motdepasseuser;";
+            // Requête SQL pour récupérer les noms
+            string query = "SELECT nom FROM developpeur";
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    lstValeurs.Items.Add(reader["nom"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
+
         }
     }
 }
